c77d4e262fcfff4234595aac489595d16fcd28a9
Samat1504 <Motivate.yourself111@gmail.com>
2022-11-17 18:13:50 +0600

    home

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Products from "./pages/Products";
 import Users from "./pages/Users";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Prices from "./pages/Prices";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<PrivateRoutes />}>
-          <Route path="/category" element={<Category />} exact={true} />
+          <Route path="/category" element={<Category />} />
           <Route path="/products" element={<Products />} />
           <Route path="/users" element={<Users />} />
           <Route path="/prices" element={<Prices />} />
         </Route>
+        <Route path="/" element={<Home  />} exact={true} />
         <Route path="/login" element={<Authorisation />} />
       </Routes>
     </Router>
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Authorization from "./Authorization"
+
+function Home() {
+	return (
+		<>
+			<Authorization  />
+		</>
+	)
+}
+
+export default Home
